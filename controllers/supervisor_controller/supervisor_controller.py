c642e23bf5cfac562d3bb3aa515c5ad466cd7ff4
Diego Carapia da Costa <diego.carapia@ufba.br>
2025-01-28 13:37:56 -0300

    adicionado movimento no portao

--- controllers/supervisor_controller/supervisor_controller.py
+++ controllers/supervisor_controller/supervisor_controller.py
@@ -4,10 +4,13 @@ TIME_STEP = 32
 
 robot = Supervisor()
 
-pioneer = robot.getFromDef('pioneer')
+wall_node = robot.getFromDef('WALL')
+translation_field = wall_node.getField('translation')
 
 while robot.step(TIME_STEP) != -1:
-    position = pioneer.getPosition()
-    x = position[0]
-    y = position[1]
-    # print(f"supervisor: x {x} y {y}")
+    
+    position = wall_node.getPosition()
+    print(position[2])
+    if (position[2] >= 0):
+        new_value = [-8.43 , 3.92, position[2] - 0.004]
+        translation_field.setSFVec3f(new_value)
